2dc7a73feb6b990a9391396255115870326919a8
rry123 <rajy25122@gmail.com>
2025-07-10 14:05:34 +0530

    Added

---
+++ user/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const userRoutes = require('./routes/user.routes');
+const cookieParser = require('cookie-parser')
+const connect = require('./db/db');
+const rabbitMq = require('./service/rabbit');
+rabbitMq.connect();
+connect();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser());
+app.use('/', userRoutes);
+
+
+module.exports = app;
